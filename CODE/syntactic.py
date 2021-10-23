98161654ccd694bf678aadd74a550398977c1783
VladoCC <vladoccplaym@gmail.com>
2021-10-23 11:53:41 +1000

    Get rid of docs in parser

--- CODE/syntactic.py
+++ CODE/syntactic.py
@@ -4,10 +4,6 @@ import re
 
 
 class Rule(object):
-	"""
-	Represents a CFG rule.
-	"""
-
 	def __init__(self, lhs, rhs):
 		# Represents the rule 'lhs -> rhs', where lhs is a non-terminal and
 		# rhs is a list of non-terminals and terminals.
@@ -36,19 +32,11 @@ class Rule(object):
 
 
 class Grammar(object):
-	"""
-	Represents a CFG.
-	"""
-
 	def __init__(self):
 		# The rules are represented as a dictionary from L.H.S to R.H.S.
 		self.rules = defaultdict(list)
 
 	def add(self, rule):
-		"""
-		Adds the given rule to the grammar.
-		"""
-
 		self.rules[rule.lhs].append(rule)
 
 	@staticmethod
@@ -90,18 +78,9 @@ class Grammar(object):
 		return self.rules[nt]
 
 	def is_terminal(self, sym):
-		"""
-		Checks is the given symbol is terminal.
-		"""
-
 		return len(self.rules[sym]) == 0
 
 	def is_tag(self, sym):
-		"""
-		Checks whether the given symbol is a tag, i.e. a non-terminal with rules
-		to solely terminals.
-		"""
-
 		if not self.is_terminal(sym):
 			return all(self.is_terminal(s) for r in self.rules[sym] for s in
 			           r.rhs)
@@ -110,10 +89,6 @@ class Grammar(object):
 
 
 class EarleyState(object):
-	"""
-	Represents a state in the Earley algorithm.
-	"""
-
 	GAM = '<GAM>'
 
 	def __init__(self, rule, dot=0, sent_pos=0, chart_pos=0, back_pointers=[]):
@@ -153,34 +128,18 @@ class EarleyState(object):
 		        ' (' + ', '.join(str_helper(s) for s in self.back_pointers) + ')')
 
 	def next(self):
-		"""
-		Return next symbol to parse, i.e. the one after the dot
-		"""
-
 		if self.dot < len(self):
 			return self.rule[self.dot]
 
 	def is_complete(self):
-		"""
-		Checks whether the given state is complete.
-		"""
-
 		return len(self) == self.dot
 
 	@staticmethod
 	def init():
-		"""
-		Returns the state used to initialize the chart in the Earley algorithm.
-		"""
-
 		return EarleyState(Rule(EarleyState.GAM, ['S']))
 
 
 class ChartEntry(object):
-	"""
-	Represents an entry in the chart used by the Earley algorithm.
-	"""
-
 	def __init__(self, states):
 		# List of Earley states.
 		self.states = states
@@ -198,19 +157,11 @@ class ChartEntry(object):
 		return '\n'.join(str(s) for s in self.states)
 
 	def add(self, state):
-		"""
-		Add the given state (if it hasn't already been added).
-		"""
-
 		if state not in self.states:
 			self.states.append(state)
 
 
 class Chart(object):
-	"""
-	Represents the chart used in the Earley algorithm.
-	"""
-
 	def __init__(self, entries):
 		# List of chart entries.
 		self.entries = entries
@@ -230,20 +181,11 @@ class Chart(object):
 
 	@staticmethod
 	def init(l):
-		"""
-		Initializes a chart with l entries (Including the dummy start state).
-		"""
-
 		return Chart([(ChartEntry([]) if i > 0 else
 		               ChartEntry([EarleyState.init()])) for i in range(l)])
 
 
 class EarleyParser(object):
-	"""
-	Represents the Earley-generated parse for a given sentence according to a
-	given grammar.
-	"""
-
 	def __init__(self, sentence, grammar=Grammar.load_grammar("grammar\\grammar.txt")):
 		self.words = sentence.split()
 		self.grammar = grammar
@@ -251,19 +193,11 @@ class EarleyParser(object):
 		self.chart = Chart.init(len(self.words) + 1)
 
 	def predictor(self, state, pos):
-		"""
-		Earley Predictor.
-		"""
-
 		for rule in self.grammar[state.next()]:
 			self.chart[pos].add(EarleyState(rule, dot=0,
 			                                sent_pos=state.chart_pos, chart_pos=pos))
 
 	def scanner(self, state, pos):
-		"""
-		Earley Scanner.
-		"""
-
 		if state.chart_pos < len(self.words):
 			word =  self.words[pos] if len(self.words) > pos else ""
 
@@ -273,10 +207,6 @@ class EarleyParser(object):
 				                                    chart_pos=(state.chart_pos)))
 
 	def completer(self, state, pos):
-		"""
-		Earley Completer.
-		"""
-
 		for prev_state in self.chart[state.chart_pos]:
 			if prev_state.next() == state.rule.lhs:
 				self.chart[pos].add(EarleyState(prev_state.rule,
@@ -285,11 +215,6 @@ class EarleyParser(object):
 				                                back_pointers=(prev_state.back_pointers + [state])))
 
 	def parse(self):
-		"""
-		Parses the sentence by running the Earley algorithm and filling out the
-		chart.
-		"""
-
 		def is_terminal(state):
 			return self.grammar.is_terminal(state.next())
 
@@ -304,10 +229,6 @@ class EarleyParser(object):
 					self.completer(state, i)
 
 	def has_parse(self):
-		"""
-		Checks whether the sentence has a parse.
-		"""
-
 		for state in self.chart[-1]:
 			if state.is_complete() and state.rule.lhs == 'S' and \
 				state.sent_pos == 0 and state.chart_pos == len(self.words):
@@ -316,10 +237,6 @@ class EarleyParser(object):
 		return False
 
 	def get(self):
-		"""
-		Returns the parse if it exists, otherwise returns None.
-		"""
-
 		def get_helper(state):
 			if len(state.back_pointers) == 0:
 				return Tree(state.rule.lhs, [state.rule.rhs[0]])
