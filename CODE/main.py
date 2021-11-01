29d2f54e8a3001713b05b98d8ad11ec82943ef3f
VladoCC <vladoccplaym@gmail.com>
2021-11-01 17:40:03 +1000

    Make output of main representable

--- CODE/main.py
+++ CODE/main.py
@@ -6,6 +6,8 @@ from lexical import LexicalAnalyzer
 from syntactic import EarleyParser
 from semantic import SemanticAnalyzer, SemanticError
 
+from lupa import LuaRuntime
+
 analyzer = LexicalAnalyzer()
 
 def translate(code):
@@ -15,14 +17,15 @@ def translate(code):
 		token_line += token.as_symbol() + " "
 	token_line = token_line[:-1]
 
-	print(token_line)
+	#print(token_line)
 
 	parser = EarleyParser(tokens)
 	parser.parse()
 	tree = parser.get()
 
 	if tree is not None:
-		tree.pretty_print()
+		#tree.pretty_print()
+		pass
 	else:
 		print("can't parse code: \n", code)
 
@@ -39,6 +42,13 @@ if __name__ == '__main__':
 	files = [join("input", file) for file in listdir("input") if isfile(join("input", file)) and file.endswith(".py")]
 	for file in files:
 		py_code = open(file, "r").read()
+		print("Python code: \n", py_code)
+		print("Python execution output:")
+		exec (py_code)
 		lua_code = translate(py_code)
+		print("\nLua code:")
 		print(lua_code)
+		lua_runtime = LuaRuntime()
+		print("\nLua execution output:")
+		lua_runtime.execute(lua_code)
 		open("output/" + file[5:-2] + "lua", "w").write(lua_code)
