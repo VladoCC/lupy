1473eafd9334ef4c125b953ca33cb678bd267e03
VladoCC <vladoccplaym@gmail.com>
2021-11-06 14:25:14 +1000

    Remove runtime tests

--- CODE/main.py
+++ CODE/main.py
@@ -1,7 +1,6 @@
 from os import listdir
 from os.path import isfile, join
 
-import execution
 from generator import generate
 from lexical import LexicalAnalyzer
 from syntactic import EarleyParser
@@ -39,22 +38,6 @@ def main():
 		
 		print("Status:", "SUCCESS" if error is None else "ERROR")
 		if error is None:
-			try:
-				py_output = execution.execute_python(py_code)
-				print("Python code executed successfully")
-			except BaseException as e:
-				py_output = e
-				print("Python code executed with exception:", e)
-			
-			try:
-				lua_output = execution.execute_lua(lua_code)
-				print("Lua code executed successfully")
-			except BaseException as e:
-				lua_output = e
-				print("Lua code executed with exception:", e)
-			
-			if not isinstance(py_output, RuntimeError) and not isinstance(py_output, RuntimeError):
-				print("Outputs are equal:", py_output == lua_output)
 			filename = "output/" + file[6:-2] + "lua"
 			print("Lua code was saved to:", filename)
 			open(filename, "w").write(lua_code)
